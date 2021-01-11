ce6149ba15c320833efd8eacb2d55435c07f9513
The Lazy PHP <56831662+thelazyphp@users.noreply.github.com>
2021-01-11 19:37:11 +0300

    Update rules.js

--- services/validator/rules.js
+++ services/validator/rules.js
@@ -12,9 +12,12 @@ import isUndefined from './helpers/isUndefined'
 
 export default {
   filled: ({ value }) => !isEmpty(value),
+  regex: ({ value, parameters }) => isString(parameters[0]) && parameters[0].test(value),
   string: ({ value }) => isString(value),
   email: ({ value }) => (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value)),
   min: ({ value, parameters }) => isNumeric(parameters[0]) && getSize(value) >= Number(parameters[0]),
   max: ({ value, parameters }) => isNumeric(parameters[0]) && getSize(value) <= Number(parameters[0]),
+  in: ({ value, parameters }) => parameters.includes(value),
+  accepted: ({ value }) => ['yes', 'on', 1, true].includes(value),
   confirmed: ({ value, data, attribute }) => Object.keys(data).includes(`${attribute}_confirmation`) && value === data[`${attribute}_confirmation`]
 }
